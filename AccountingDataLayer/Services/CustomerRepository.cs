cce670047a5672a739614c96c2a6a80f0e77875f
Alireza Golbad <63424607+Alireza-Golbad@users.noreply.github.com>
2021-05-31 20:52:54 +0430

    Data Layer completed

---
+++ AccountingDataLayer/Services/CustomerRepository.cs
@@ -0,0 +1,86 @@
+﻿using AccountingDataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingDataLayer.Services
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        Accounting_DBEntities db = new Accounting_DBEntities();
+        public bool DeleteCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Deleted;
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(int customerID)
+        {
+            try
+            {
+                var customer = GetCustomersById(customerID);
+                DeleteCustomer(customer);
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+
+        public List<Customers> GetAllCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customers GetCustomersById(int customerID)
+        {
+            return db.Customers.Find(customerID);
+        }
+
+        public bool InsertCustomer(Customers customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                return true;
+            }
+            catch 
+            {
+
+                return false;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public bool UpdateCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                return true;
+            }
+            catch 
+            {
+
+                return false;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using AccountingDataLayer;
+using AccountingDataLayer.Repositories;
+using AccountingDataLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerRepository customer = new CustomerRepository();
+
+            Customers addCustomer = new Customers()
+            {
+                FullName = "احمد تمیزی",
+                Mobile = "09365531447",
+                CustomerImage = "Nono"
+            };
+            customer.InsertCustomer(addCustomer);
+            customer.Save();
+
+            var list = customer.GetAllCustomers();
+        }
+    }
+}
